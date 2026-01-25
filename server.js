40f4d2a0be681125d85c54e263764d7c1333fdba
AKOLANGTOSINATORY <84948127+AKOLANGTOSINATORY@users.noreply.github.com>
2026-01-26 03:02:19 +0800

    Update server.js

--- server.js
+++ server.js
@@ -11,13 +11,6 @@ app.get("/message", (req, res) => {
 app.get("/", (req, res) => {
     const whitelistData = [
         { username: "SeintSlavx",gameCreatorId: "851145815", WhitelistEnabled: true, Suspended: false },
-        { username: "|CAF| Commonwealth Armed Forces",gameCreatorId: "33313680", WhitelistEnabled: true, Suspended: false }, // february 9
-        { username: "Philippine Army | ROBLOX",gameCreatorId: "15114508", WhitelistEnabled: true, Suspended: false }, // april 20
-        { username: "| -RAF- | Russian Armed Forces",gameCreatorId: "706574002", WhitelistEnabled: true, Suspended: false }, // jan 4 extended nato
-        { username: "Fort Musk Military Academy",gameCreatorId: "32334049", WhitelistEnabled: true, Suspended: false },
-        { username: "|GEA| Glorioso Ejército Argentino",gameCreatorId: "10114226", WhitelistEnabled: true, Suspended: false },
-        { username: "UKA | United Kingdoms Army",gameCreatorId: "35519871", WhitelistEnabled: true, Suspended: false }, // 3days
-        { username: " -[MPA]- The Portuguese Army!-",gameCreatorId: "15976392", WhitelistEnabled: true, Suspended: false },
 
     ];
     res.json(whitelistData); // Return the whitelist with the enabled and suspended status for each user
