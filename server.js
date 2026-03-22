b1cd74c1e20b6cd36d9f43eae0eb61a617bc88bf
AKOLANGTOSINATORY <84948127+AKOLANGTOSINATORY@users.noreply.github.com>
2026-03-23 04:18:39 +0800

    Modify whitelist user statuses and comments
    Updated whitelist data to reflect changes in user status.

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ app.get("/message", (req, res) => {
 app.get("/", (req, res) => {
     const whitelistData = [
         { username: "SeintSlavx",gameCreatorId: "851145815", WhitelistEnabled: true, Suspended: false },
-        { username: "| USDF | United States Defense Forces",gameCreatorId: "34814858", WhitelistEnabled: true, Suspended: false },
-        { username: "Sormovsky Underground Combat Syndicate",gameCreatorId: "590047336", WhitelistEnabled: true, Suspended: false },
-        { username: "USM 1960's, Vietnam War",gameCreatorId: "991095116", WhitelistEnabled: true, Suspended: false },
-        { username: "Snake Industry Team",gameCreatorId: "8459375", WhitelistEnabled: true, Suspended: false },
+        { username: "| USDF | United States Defense Forces",gameCreatorId: "34814858", WhitelistEnabled: false, Suspended: false }, // expired march 23
+        { username: "Sormovsky Underground Combat Syndicate",gameCreatorId: "590047336", WhitelistEnabled: true, Suspended: false }, // april 4
+        { username: "USM 1960's, Vietnam War",gameCreatorId: "991095116", WhitelistEnabled: true, Suspended: false }, // March 14
+        { username: "Snake Industry Team",gameCreatorId: "8459375", WhitelistEnabled: true, Suspended: false }, // March 17
 
     ];
     res.json(whitelistData); // Return the whitelist with the enabled and suspended status for each user
