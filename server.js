140d582cc724119caf9e978c9c228b6161eea32f
AKOLANGTOSINATORY <84948127+AKOLANGTOSINATORY@users.noreply.github.com>
2026-03-31 10:13:16 +0800

    Update whitelist status for multiple users

--- server.js
+++ server.js
@@ -13,9 +13,10 @@ app.get("/", (req, res) => {
         { username: "SeintSlavx",gameCreatorId: "851145815", WhitelistEnabled: true, Suspended: false },
         { username: "| USDF | United States Defense Forces",gameCreatorId: "34814858", WhitelistEnabled: false, Suspended: false }, // expired march 23
         { username: "Sormovsky Underground Combat Syndicate",gameCreatorId: "590047336", WhitelistEnabled: true, Suspended: false }, // april 4
-        { username: "USM 1960's, Vietnam War",gameCreatorId: "991095116", WhitelistEnabled: true, Suspended: false }, // March 14
+        { username: "USM 1960's, Vietnam War",gameCreatorId: "991095116", WhitelistEnabled: false, Suspended: false }, // March 14
         { username: "1lNNS",gameCreatorId: "4583397891", WhitelistEnabled: true, Suspended: false }, // Active 15
-        { username: "Snake Industry Team",gameCreatorId: "8459375", WhitelistEnabled: true, Suspended: false }, // March 17
+        { username: "Snake Industry Team",gameCreatorId: "8459375", WhitelistEnabled: false, Suspended: false }, // March 17
+        { username: "[BRA] - British Royal Army",gameCreatorId: "901380083", WhitelistEnabled: true, Suspended: false }, // April 31
 
     ];
     res.json(whitelistData); // Return the whitelist with the enabled and suspended status for each user
