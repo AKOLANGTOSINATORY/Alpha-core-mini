359d0a6065075a8fd48838f14f4f005b3e3b0529
AKOLANGTOSINATORY <84948127+AKOLANGTOSINATORY@users.noreply.github.com>
2026-04-19 04:09:17 +0800

    Clean up whitelist data by removing expired entries
    Removed expired entries from the whitelist data.

--- server.js
+++ server.js
@@ -11,11 +11,7 @@ app.get("/message", (req, res) => {
 app.get("/", (req, res) => {
     const whitelistData = [
         { username: "SeintSlavx",gameCreatorId: "851145815", WhitelistEnabled: true, Suspended: false },
-        { username: "| USDF | United States Defense Forces",gameCreatorId: "34814858", WhitelistEnabled: false, Suspended: false }, // expired march 23
         { username: "Sormovsky Underground Combat Syndicate",gameCreatorId: "590047336", WhitelistEnabled: true, Suspended: false }, // april 4
-        { username: "USM 1960's, Vietnam War",gameCreatorId: "991095116", WhitelistEnabled: false, Suspended: false }, // March 14
-        { username: "1lNNS",gameCreatorId: "4583397891", WhitelistEnabled: true, Suspended: false }, // Active 15
-        { username: "Snake Industry Team",gameCreatorId: "8459375", WhitelistEnabled: false, Suspended: false }, // March 17
         { username: "[BRA] - British Royal Army",gameCreatorId: "901380083", WhitelistEnabled: true, Suspended: false }, // April 31
         { username: "• [BA] British Army •",gameCreatorId: "16970985", WhitelistEnabled: true, Suspended: false }, // May 3
         { username: "|BA| British Army!.",gameCreatorId: "35099271", WhitelistEnabled: true, Suspended: false }, // May 7
