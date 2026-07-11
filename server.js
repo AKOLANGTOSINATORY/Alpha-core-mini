6e62feda17b22c451f1574471ad7c2246fc6fe0e
AKOLANGTOSINATORY <84948127+AKOLANGTOSINATORY@users.noreply.github.com>
2026-07-11 19:36:09 +0800

    Update whitelist status for multiple users

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ app.get("/", (req, res) => {
     const whitelistData = [
         { username: "SeintSlavx",gameCreatorId: "851145815", WhitelistEnabled: true, Suspended: false },
         { username: "French Armed Forceṡ",gameCreatorId: "363834904", WhitelistEnabled: false, Suspended: false }, // May 20
-        { username: "Riverstone Prison",gameCreatorId: "33851205", WhitelistEnabled: true, Suspended: false }, // june 28
-        { username: "Florenta State Prison",gameCreatorId: "955353315", WhitelistEnabled: true, Suspended: false }, // june 8
-        { username: "|| British Army",gameCreatorId: "704324852", WhitelistEnabled: true, Suspended: false }, // june 25
-        { username: "|SBA| Skad's British Army",gameCreatorId: "32540278", WhitelistEnabled: true, Suspended: false }, // JUNE 26
-        { username: "|СA| Canadian Army",gameCreatorId: "144649153", WhitelistEnabled: true, Suspended: false }, // June 30
-        { username: "BARP | British Army Roleplay",gameCreatorId: "1087800622", WhitelistEnabled: true, Suspended: false }, // July 9
-
+        { username: "Riverstone Prison",gameCreatorId: "33851205", WhitelistEnabled: false, Suspended: false }, // june 28
+        { username: "Florenta State Prison",gameCreatorId: "955353315", WhitelistEnabled: false, Suspended: false }, // june 8
+        { username: "|| British Army",gameCreatorId: "704324852", WhitelistEnabled: false, Suspended: false }, // june 25
+        { username: "|SBA| Skad's British Army",gameCreatorId: "32540278", WhitelistEnabled: false, Suspended: false }, // JUNE 26
+        { username: "|СA| Canadian Army",gameCreatorId: "144649153", WhitelistEnabled: false, Suspended: false }, // June 30
+        { username: "BARP | British Army Roleplay",gameCreatorId: "1087800622", WhitelistEnabled: false, Suspended: false }, // July 9
+        { username: "[VRR] British Army",gameCreatorId: "35769284", WhitelistEnabled: true, Suspended: false }, // Aug 11
     ];
     res.json(whitelistData); // Return the whitelist with the enabled and suspended status for each user
 });
